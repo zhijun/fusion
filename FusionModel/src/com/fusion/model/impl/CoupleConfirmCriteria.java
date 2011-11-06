f65f4513c6d2d5804dbc8b9df5ff71f51c42dda2
Zhijun Cao <caozhijun@cn.ibm.com>
2011-11-06 23:31:44 +0800

    first criteria instance

---
+++ FusionModel/src/com/fusion/model/impl/CoupleConfirmCriteria.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.fusion.model.impl;
+
+import com.fusion.model.core.Criteria;
+
+/**
+ * @author zhijun
+ *
+ */
+public class CoupleConfirmCriteria extends Criteria {
+
+	/**
+	 * 
+	 */
+	public CoupleConfirmCriteria() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.fusion.model.core.Criteria#isMet()
+	 */
+	@Override
+	public boolean isMet() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
