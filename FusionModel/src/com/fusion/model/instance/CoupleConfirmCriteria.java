4660eb7810f49923f26619520aaf966b7b07a603
Zhijun Cao <caozhijun@cn.ibm.com>
2011-11-06 23:34:32 +0800

    package name tweak

---
+++ FusionModel/src/com/fusion/model/instance/CoupleConfirmCriteria.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.fusion.model.instance;
+
+import com.fusion.model.core.Criteria;
+
+/**
+ * @author zhijun
+ *
+ */
+public class CoupleConfirmCriteria extends Criteria {
+
+	/**
+	 * 
+	 */
+	public CoupleConfirmCriteria() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.fusion.model.core.Criteria#isMet()
+	 */
+	@Override
+	public boolean isMet() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
