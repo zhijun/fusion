b414646db2567ed95927e59a9a45115279ed9c2e
hp <hp@Yjmnrg>
2012-02-11 14:14:57 +0800

    fisrt new project
    Signed-off-by: hp <hp@Yjmnrg>

---
+++ Fengche/src/com/fusion/fengche/FengcheActivity.java
@@ -0,0 +1,13 @@
+package com.fusion.fengche;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FengcheActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
